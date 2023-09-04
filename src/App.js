0bd5bcb468a26c642002cb45b0a9b61f6e619e4b
unknown <jdorgu011@aol.com>
2023-09-04 03:11:49 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header';
+import Footer from "./components/footer";
 
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header /> 
+      <Footer />
     </div>
   );
 }
